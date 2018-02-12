c6795d8fe675936b0cb800dbed0677569bbd9ea7
bechitra <bechitra@outlook.com>
2018-02-13 00:16:51 +0600

    Welcome Screen Created

--- app/src/main/java/com/example/bechitra/walleto/MainActivity.java
+++ app/src/main/java/com/example/bechitra/walleto/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.bechitra.walleto;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout myLayout;
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myLayout = findViewById(R.id.mainActivityLayout);
+        animationDrawable = (AnimationDrawable) myLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();
     }
 }
